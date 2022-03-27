09d676fe8b41d0b6f8b45467732414a20eee68ec
ClaudioSarmento <claudio-s-i@hotmail.com>
2022-03-27 14:35:56 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -13,9 +13,24 @@ namespace xadrex_console
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                Tela.imprimirTabuleiro(partida.tab);
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Console.WriteLine("===== JOGO DE XADREZ =====");
+                    Console.WriteLine();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-            }catch(TabuleiroException e)
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.ExecutaMovimento(origem, destino);
+                }
+            }
+            catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
